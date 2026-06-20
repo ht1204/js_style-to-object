e9e3c2224dca880c5faefdacea8a8fb590d622ec
Hector Triana <ptb1204@hotmail.com>
2026-06-20 17:54:45 -0500

    feat: proposal approach style-object

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -6,7 +6,21 @@
  * @return {object}
  */
 function convertToObject(sourceString) {
-  // write your code here
+  const parsedStyles = {};
+
+  sourceString
+    .split(';')
+    .map((declaration) => declaration.trim())
+    .filter((declaration) => declaration.length > 0)
+    .forEach((declaration) => {
+      const colonIndex = declaration.indexOf(':');
+      const property = declaration.slice(0, colonIndex).trim();
+      const value = declaration.slice(colonIndex + 1).trim();
+
+      parsedStyles[property] = value;
+    });
+
+  return parsedStyles;
 }
 
 module.exports = convertToObject;
